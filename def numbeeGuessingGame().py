c09b33163c8a77e31c8c21f7f16cdeb3b4192fba
NikunjDharawat <74286044+NikunjDharawat@users.noreply.github.com>
2022-01-25 22:52:30 +0530

    Add files via upload

---
+++ def numbeeGuessingGame().py
@@ -0,0 +1,14 @@
+def  numbeeGuessingGame()
+     number = input("enter the number:")
+ 
+    while chances <5
+    if guess == number:
+    print("Congratulations You Won!!!")
+    break
+
+    if not chances <5
+    print("You Lose !!1 the number is",number)
+    print("guess a number (between 1 and 9):")
+    print(guess a number (between 1 and 9))
+
+countWordsFromFile()
